b1925783045075c725747c0dd7959ea0ad414dc0
jiaxi9427 <Peng.jia@northeastern.edu>
2024-03-03 22:10:27 -0500

    update3/3

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -1,42 +1,29 @@
-const aliceTumbling = [
+async function animateAlice() {
+  const aliceTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
   ];
-  
+
   const aliceTiming = {
     duration: 2000,
     iterations: 1,
     fill: 'forwards'
-  }
-  
+  };
+
   const alice1 = document.querySelector("#alice1");
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    console.log("Alice1 animation finished");
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    console.log("Alice2 animation finished");
+    await alice3.animate(aliceTumbling, aliceTiming).finished;
+    console.log("Alice3 animation finished");
+  } catch (err) {
+    alert(`Error when animating: ${err.message}`);
+  }
+}
 
-  // Promise chain  
-  // alice1.animate(aliceTumbling, aliceTiming).finished  
-  //   .then(() => {
-  //       return alice2
-  //               .animate(aliceTumbling, aliceTiming)
-  //               .finished;     
-  //   })
-  //   .then(() => {
-  //     return alice3
-  //             .animate(aliceTumbling, aliceTiming)
-  //             .finished;
-  //   })
-  //   .catch((err) => alert(`Error when promising ... ${err.message}`));
+animateAlice();
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,24 @@
-const array2D = [
+
+async function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+  }
+  
+  async function sum2DArray(array2D) {
+    try {
+      const sums = await Promise.all(array2D.map(arr => sumArray(arr)));
+      const totalSum = sums.reduce((total, current) => total + current, 0);
+      console.log(`Total sum: ${totalSum}`);
+      return totalSum;
+    } catch (error) {
+      console.error(`Error calculating sum: ${error}`);
+    }
+  }
+  
+  const array2D = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-];
+  ];
+  
+  sum2DArray(array2D);
+  
